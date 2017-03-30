01c10e95f135266137ecd0b511738faa41b2fe5d
Denis Urakanov <mcquicklyss@gmail.com>
2017-03-31 00:04:26 +0500

    Commit 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,96 @@
+package com.company;
+
+        import org.jsoup.Jsoup;
+        import org.jsoup.nodes.Document;
+        import org.jsoup.nodes.Element;
+        import org.jsoup.select.Elements;
+
+        import java.io.IOException;
+        import java.util.ArrayList;
+        import java.util.List;
+
+public class Main {
+
+    String MAIN_URL = "http://www.susu.ru/ru/university/old-departments";
+
+    public static void main(String[] args) throws IOException {
+        List <Article> articleList = new ArrayList<>();
+
+        Main start = new Main();
+        String currentUrl = "http://www.susu.ru/ru/university/old-departments";
+        start.parseNote(currentUrl);
+
+    }
+
+    void parseNote(String URL) throws IOException {
+
+        Document doc = Jsoup.connect(URL).get();
+
+        Element d = doc.getElementById("md1");
+        Element divElement = d.child(0);
+        Elements hElements = divElement.children();
+
+
+        for (Element hElement: hElements) {
+            String check = hElement.html();
+            if ((check.compareTo("&nbsp;")==0)||(!hElement.isBlock())) continue;
+
+            Element aElement = hElement.child(0);
+            if (aElement.isBlock()||!aElement.is("a")) return;
+
+            String url = aElement.attr("abs:href");
+            String title = aElement.text();
+
+            System.out.println(url+" "+title);
+            parseNote(url);
+            //articleList.add(new Article(url, title));
+        }
+
+
+        /*for (Article a: articleList) {
+            System.out.println(a.getName()+" "+a.getUrl());
+        }*/
+    }
+
+    void findRef(Elements elements) {
+        for (Element element: elements) {
+            if(element.isBlock()) findRef(element.children());
+            else {
+                if (element.is("a")) {
+                    String url = element.attr("abs:href");
+                    String title = element.text();
+                    System.out.println(url+" "+title);
+                }
+                else return;
+            }
+        }
+
+    }
+
+    static class Article {
+
+        private String url;
+        private String name;
+
+        public Article(String url, String name) {
+            this.url = url;
+            this.name = name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
